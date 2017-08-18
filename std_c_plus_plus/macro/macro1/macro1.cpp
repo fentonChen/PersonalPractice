532d4391f59627364884ad8d9b53b6b818635c77
fentonChen <fenton.c@outlook.com>
2017-08-18 10:32:06 +0800

    added examples of macro into std_c_plus_plus

---
+++ std_c_plus_plus/macro/macro1/macro1.cpp
@@ -0,0 +1,16 @@
+//macro1.cpp
+//Function-like macros
+#include <iostream>
+using namespace std;
+
+#define MIN(a,b) (((a)<(b)) ? a : b)
+
+int main() {
+  int i, j;
+  i = 100;
+  j = 30;
+  
+  cout << "The minimum is " << MIN(i, j) << endl;
+
+  return 0;
+}
