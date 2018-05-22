0bd3e27bb926271ec19b58a88251f8f53adfadba
fengteng <fengteng@bj-m-210562a.local>
2018-05-22 14:32:08 +0800

    renamed folder std_c_plus_plus to std_cplus added a example of regex to std_cplus

---
+++ std_cplus/regex/regex1/regex_domain_replace.cpp
@@ -0,0 +1,14 @@
+//regex example of findout domain area and replace it to an IP address
+#include <regex>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+  //char *exm_url = "http://wssource.pull.inke.cn/1526438291626693.flv";
+  char *exm_url = "tcp://wssource.pull.inke.cn:80";
+  regex reg1("\\b(:\/\/)([^:\/]*)\\b"); // matches words begining by "://"
+  std::string replace_str = "://115.24.180.256";
+  std::string rebuild_str = std::regex_replace (exm_url, reg1, replace_str.c_str());
+  std::cout <<"rebuild_str = " << rebuild_str.c_str() << std::endl;
+}
